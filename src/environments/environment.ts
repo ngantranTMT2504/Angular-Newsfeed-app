f1a1ee76abfcec9b55f8650d42cc8749b1dfa866
ngantranTMT2504 <ttknganct@gmail.com>
2023-05-08 15:15:11 +0700

    update code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,4 @@
 export const environment = {
     production: false,
-    firebase: {
-        apiKey: "AIzaSyDmrr7vi_gLqnZsAMek-jY-OKWUHjzrrXY",
-        authDomain: "angular-newsfeed-48667.firebaseapp.com",
-        projectId: "angular-newsfeed-48667",
-        storageBucket: "angular-newsfeed-48667.appspot.com",
-        messagingSenderId: "385685042238",
-        appId: "1:385685042238:web:3e6bcd51eecf3c32bccd6f"
-    }
+    firebase: {}
 };
